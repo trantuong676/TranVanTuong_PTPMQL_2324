e365f2493b36435275cbcfb8c55197d52d1b9b32
trantuong676 <128342689+trantuong676@users.noreply.github.com>
2023-08-23 15:11:40 +0700

    23/08

---
+++ Testapp/Models/Test.cs
@@ -0,0 +1,12 @@
+namespace TESTAPP.Models
+{
+    public class Test
+    {
+    Test ten = new Test ();
+    Test TUOI = new Test ();
+    Test LOP = new Test ();
+    Test HO = new Test ();
+    Test NGAY= new Test ();
+        
+    }
+}
